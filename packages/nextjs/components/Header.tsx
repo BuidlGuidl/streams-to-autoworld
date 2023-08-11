05acf71e41965367a69a9b3ac27c99ca1989bca1
Austin Griffith <austin@ethereum.org>
2023-08-11 16:16:12 -0600

    AW logo

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -11,6 +11,8 @@ export const Header = () => {
       <div className="flex items-center justify-center mb-4 sm:mb-0">
         <div className="flex-col items-start">
           <Image src="/jessy.png" alt="Hacker House Logo" width={523} height={36} className="mx-4" />
+          <Image src="/aw.png" alt="AW Logo" width={523} height={36} className="mx-4" />
+
           <div className="m-0 mt-1 pl-4 text-sm md:text-base">
             <span className="pr-2">selection committee:</span>
             <a href="https://twitter.com/Komorebi8888" target="_blank" rel="noreferrer">
